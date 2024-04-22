0a6b523a0668aee76b381e1e22ff39f6661ea165
miaoiaoaoa <3356630395@qq.com>
2024-04-22 14:23:11 +0800

    no

--- cnn.py
+++ cnn.py
@@ -7,7 +7,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-
+a=0
 batch_size = 128
 num_classes = 10
 epochs = 1
